1b9947f579d95cecc933f78468c9f5d8155eb3b2
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-26 13:06:45 -0700

    added get routes for notes and langing page, added app.listen to call server.js page

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { application } = require('express');
 const express = require('express');
 const path = require ('node:path')
 const api = require('./routes/api');
@@ -11,12 +10,22 @@ const app = express();
 app.use(express.json());
 
 //do we have to use middleware for urlencoded form data?
-app.use('api/notes', api);
+app.use('/api', api);
 
 app.use(express.static('public'));
 
 
 
 //GET Route for landing page
-app.get('/', (req,res)=>
-res)
+app.get('*', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+
+//GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    );
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`));
