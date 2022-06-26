1ae2b5653a5d6f1d4ee9fba8b25978298967239c
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-26 15:05:21 -0700

    added notes post request for appending new notes into data

--- routes/api.js
+++ routes/api.js
@@ -12,4 +12,33 @@ notes.get('/api/notes', (req, res) => {
 //POST Route for a new note
 notes.post('/api/notes', (req,res) => {
     console.log(req.body)
+
+    const {title, note} = req.body;
+
+    if(req.body){
+        const newNote = {
+            title,
+            note,
+            note_id: uniqid.time(),
+        };
+    
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err){
+                console.err(err);
+            } else {
+                const parsedNote = JSON.parse(data);
+                parsedNote.push(newNote);
+                fs.writeFile('./db/db.json', JSON.stringify(newNote, null, 4), (err) =>
+                err ? console.error(err) : console.log(`\n Data written to db file!`));
+            }
+        })
+    
+    
+    
+    }
+
+
 })
+
+
+module.exports = notes;
