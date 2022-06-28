c20461d11c43a27df3d19e8d540447ade002c33e
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-27 21:19:38 -0700

    restructured files

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
 const notes = require('express').Router();
 const fs = require('fs');
-var uniqid = require('uniqid');
+var uuid = require('uuid');
 
 
 // GET Route for retrieving all notes
 notes.get('/api/notes', (req, res) => {
-   fs.readFile("./db/db.json", 'utf-8', (err, data)) 
+   fs.readFile("../db/db.json", 'utf-8', (err, data)) 
     .then((data) => res.json(JSON.parse(data)));
 })
 
@@ -14,21 +14,21 @@ notes.post('/api/notes', (req,res) => {
     console.log(req.body)
 
     const {title, note} = req.body;
-
+//creates object from input data for note to be stored with an id 
     if(req.body){
         const newNote = {
             title,
             note,
-            note_id: uniqid.time(),
+            note_id: uuid,
         };
-    
-        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    //read the file db to pull in into code then append with the new note
+        fs.readFile('../db/db.json', 'utf-8', (err, data) => {
             if (err){
                 console.err(err);
             } else {
                 const parsedNote = JSON.parse(data);
                 parsedNote.push(newNote);
-                fs.writeFile('./db/db.json', JSON.stringify(newNote, null, 4), (err) =>
+                fs.writeFile('../db/db.json', JSON.stringify(newNote, null, 4), (err) =>
                 err ? console.error(err) : console.log(`\n Data written to db file!`));
             }
         })
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require ('node:path')
 const api = require('./routes/api');
+const htmlRoutes  = require("./routes/htmlRoutes")
 
 const PORT = process.env.port || 3001;
 
@@ -9,22 +9,15 @@ const app = express();
 
 app.use(express.json());
 
-//do we have to use middleware for urlencoded form data?
-app.use('/api', api);
-
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+app.use('/api', api);
+app.use("/", htmlRoutes)
 
 
 
-//GET Route for landing page
-app.get('*', (req,res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-    );
 
 //GET Route for notes page
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-    );
 
 
 app.listen(PORT, () =>
