3830ba01cf9aab16c4380c2acebc1e49b9b16243
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-28 18:47:42 -0700

    added delete get to api.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,4 @@
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -119,6 +120,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
+  console.log(jsonNotes);
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
--- routes/api.js
+++ routes/api.js
@@ -1,43 +1,69 @@
 const notes = require('express').Router();
 const fs = require('fs');
-var uuid = require('uuid');
-
+const { v4: uuid } = require('uuid');
+let note_id;
 
 // GET Route for retrieving all notes
-notes.get('/api/notes', (req, res) => {
-   fs.readFile("../db/db.json", 'utf-8', (err, data)) 
-    .then((data) => res.json(JSON.parse(data)));
-})
+notes.get('/notes', (req, res) => {
+    fs.readFile("db/db.json", (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        res.json(JSON.parse(data))
+    })
+}
+)
+
 
 //POST Route for a new note
-notes.post('/api/notes', (req,res) => {
+notes.post('/notes', async (req, res) => {
     console.log(req.body)
 
-    const {title, note} = req.body;
-//creates object from input data for note to be stored with an id 
-    if(req.body){
+    const { title, text } = req.body;
+    //creates object from input data for note to be stored with an id 
+    if (req.body) {
         const newNote = {
             title,
-            note,
-            note_id: uuid,
+            text,
+            id: uuid(),
         };
-    //read the file db to pull in into code then append with the new note
-        fs.readFile('../db/db.json', 'utf-8', (err, data) => {
-            if (err){
+        //read the file db to pull in into code then append with the new note
+        fs.readFile('db/db.json', 'utf8', (err, data) => {
+            if (err) {
                 console.err(err);
             } else {
                 const parsedNote = JSON.parse(data);
                 parsedNote.push(newNote);
-                fs.writeFile('../db/db.json', JSON.stringify(newNote, null, 4), (err) =>
-                err ? console.error(err) : console.log(`\n Data written to db file!`));
+                fs.writeFile('db/db.json', JSON.stringify(parsedNote, null, 4), (err, data) =>
+
+                    err ? console.error(err) : res.json(data)
+
+                )
             }
         })
-    
-    
-    
+
+
+
     }
 
 
+})
+
+notes.delete(`/notes/:id`, (req, res) => {
+    console.log(req.params.id);
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.err(err);
+        } else {
+            const parsedNote = JSON.parse(data);
+            const filteredNoteArray = parsedNote.filter(note => req.params.id !== note.id)
+            fs.writeFile('db/db.json', JSON.stringify(filteredNoteArray, null, 4), (err, data) =>
+
+                err ? console.error(err) : res.json(data)
+
+            )
+        }
+    })
 })
 
 
